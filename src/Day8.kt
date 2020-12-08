a9972fcbc966f0f70935b1fc67419843464dc1d6
wolffischer82 <fischer.wolf@outlook.de>
2020-12-08 07:20:18 +0100

    Day 8 Part 2 brute force...

--- src/Day8.kt
+++ src/Day8.kt
@@ -1,10 +1,10 @@
 class Day8(filename : String) {
     var input = listOf<String>();
     var codeExecutionMap = mutableMapOf<Int, String>();
+    var successfulFinished = false;
     var acc = 0;
-
     init {
-        input = readFile(filename);
+        input = readFile(filename).toMutableList();
 
     }
 
@@ -16,13 +16,21 @@ class Day8(filename : String) {
         return  accVal;
     }
 
-    fun execute()
+    fun execute(){
+        execute(input);
+        println ("Last acc value is $acc");
+    }
+
+    fun execute(code : List<String>)
     {
+        acc = 0;
         var currentLine = 0;
-        while (!codeExecutionMap.containsKey(currentLine))
+        successfulFinished = false;
+        codeExecutionMap.clear();
+        while (currentLine < code.size && !codeExecutionMap.containsKey(currentLine))
         {
-            codeExecutionMap[currentLine] = input[currentLine];
-            val splitCommand = input[currentLine].split(" ");
+            codeExecutionMap[currentLine] = code[currentLine];
+            val splitCommand = code[currentLine].split(" ");
             when {
                 splitCommand[0] == "nop" -> {currentLine++}
                 splitCommand[0] == "acc" -> {
@@ -34,11 +42,28 @@ class Day8(filename : String) {
                 }
             }
         }
-        println ("Last acc value is $acc");
+        if (currentLine == code.size)
+        {
+            successfulFinished = true;
+        }
     }
 
     fun executePart2() {
-
+        for (i in 0..input.size-1)
+        {
+            var copy = input.toMutableList();
+            if (input[i].contains("nop")){
+                copy[i] = input[i].replace("nop", "jmp");
+            }
+            if (input[i].contains("jmp")){
+                copy[i] = input[i].replace("jmp", "nop");
+            }
+            execute(copy);
+            if (successfulFinished) {
+                println("Successful finished with Acc value $acc");
+                break;
+            }
+        }
     }
 }
 
@@ -47,5 +72,4 @@ fun main(args : Array<String>)
     val day8 = Day8("src/Day8Input");
     day8.execute();
     day8.executePart2();
-
 }
