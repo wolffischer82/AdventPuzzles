d5b57cd784d965b31f4b9c365e9c4d2948420e48
wolffischer82 <fischer.wolf@outlook.de>
2020-12-09 17:10:51 +0100

    Day 9 Part 2

--- src/Day9.kt
+++ src/Day9.kt
@@ -5,19 +5,36 @@ class Day9 (filename : String) {
     init {
         val input = readFile(filename)
         numberList = input.map { i -> i.toLong() }.toMutableList()
+        var wrongNumber : Long = 0;
 
-
-        for (i in preambleSize..numberList.size-1){
+        for (i in preambleSize until numberList.size){
             val tempMap : MutableSet<Long> = mutableSetOf();
             numberList.subList(i-preambleSize, i).forEach { tempMap.add(numberList[i]-it)};
             var isCorrect = false;
             numberList.subList(i-preambleSize, i).forEach { if (tempMap.contains(it)) isCorrect = true }
             if (!isCorrect)
             {
+                wrongNumber = numberList[i];
                 println("Wrong element is ${numberList[i]}")
                 break;
             }
         }
+
+        var resultPart2 : Long = 0;
+loop@   for (i in 0 until numberList.size){
+            for (i2 in i+1 until numberList.size){
+                val sum = numberList.subList(i, i2).sum()
+                if (sum == wrongNumber){
+                    numberList.subList(i,i2).run { resultPart2 = minOrNull()!!.plus(this.maxOrNull()!!) };
+                    break@loop
+                }
+                if (sum > wrongNumber){
+                    continue@loop
+                }
+
+            }
+        }
+        println("Result part2 $resultPart2")
     }
 }
 
