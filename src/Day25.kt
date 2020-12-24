9276d151a7f6c121af86d242c36d39e0dff2039f
wolffischer82 <fischer.wolf@outlook.de>
2020-12-24 08:51:25 +0100

    Prep for Day25

---
+++ src/Day25.kt
@@ -0,0 +1,13 @@
+class Day25 (filename : String) {
+    val filename = filename
+    fun part1(){
+        val input = readFile(filename)
+    }
+    fun part2(){
+
+    }
+}
+
+fun main (args : Array<String>){
+    Day25("src/Day25Input").run { part1(); part2() }
+}
